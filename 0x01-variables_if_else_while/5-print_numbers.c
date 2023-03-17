cd382b694ec0757abf1296fd34862187a5c111f5
Chr400 <hananchrgui21@gmail.com>
2023-03-17 12:02:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print all single digt numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+putchar('\n');
+return (0);
+}
