5ccf565aa20207b34157c0f9c4b052ebad775961
Chr400 <hananchrgui21@gmail.com>
2023-03-17 12:04:50 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * print the alphabet in lowercase
+ * Return: 0 if code executed successfuly
+ */
+int main(void)
+{
+int a;
+a = 0;
+while (a < 10)
+{
+putchar(a + '0');
+a++;
+}
