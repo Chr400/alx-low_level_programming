eecdf0743e04bce37db4c7769b698526554076d4
Chr400 <hananchrgui21@gmail.com>
2023-04-06 13:58:13 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+void _puts_recursion(char *s);
+int _putchar(char c);
+void rev_printer(char *s, int n);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int counter(int count, char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int halp(int c, int i);
+int hai(int n, int d);
+int is_prime_number(int n);
+int _sqrt(int x, int i);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
